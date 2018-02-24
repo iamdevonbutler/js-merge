403c37e573a8d4ff943d10e5e3439c0eb90347d1
iamdevonbutler <jpescione@gmail.com>
2018-02-24 11:01:41 -0500

    working on deep equal

--- tests/main.test.js
+++ tests/main.test.js
@@ -4,27 +4,42 @@ const should = require('chai').should();
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 
+// @todo compare arrays too.
+// dates .getTime() instanceof Date
+// what happens when given no params or just one.
+// rename js-package-name
 function deepEqual(...objs) {
   var equal = true;
-  function compare(obj, obj1) {
+  function compareArrays(obj, obj1) {
+    let equal = true;
+    obj[key].forEach((item, i) => {
+      if (isObject(obj[key])) {
+        if (!deepEqual(obj[key], obj1[key])) {
+          equal = false;
+        }
+      }
+      else if (obj[key] !== obj1[key]) {
+        equal = false;
+      }
+    });
+  }
+  function compareObjects(obj, obj1) {
     for (let key in obj) {
       if (obj1[key] === undefined) {
         return false;
       }
       if (isArray(obj[key])) {
-        let equal = true;
-        obj[key].forEach((item, i) => {
-          if ((isArray(obj[key]) || isObject(obj[key])) && !deepEqual(obj[key], obj1[key])) {
-            equal = false;
-          }
-          else if (obj[key] !== obj1[key]) {
-            equal = false;
-          }
-        });
-        if (!equal) return false;
+        if (!compareArrays(obj[key], obj1[key])) {
+          return false;
+        }
+      }
+      else if (isDate(obj[key])) {
+        if (!(obj1[key] instanceof Date) || obj[key].getTime() !== obj1[key].getTime()) {
+          return false;
+        }
       }
       else if (isObject(obj[key])) {
-        if (!deepEqual(obj[key], obj1[key])) {
+        if (!compareObjects(obj[key], obj1[key])) {
           return false;
         }
       }
@@ -40,7 +55,17 @@ function deepEqual(...objs) {
     return true;
   }
   objs.forEach((obj, i) => {
-    if (objs[i+1] !== undefined && !compare(objs[i], objs[i+1])) {
+    if (objs[i+1] !== undefined) {
+      if (isArray(objs[i])) {
+        if (!isArray(objs[i+1]) || !compareArrays(objs[i], objs[i+1]) {
+          equal = false;
+        }
+      }
+      else if (isObject(objs[i])) {
+        if (!isObject(objs[i+1]) || !compareObjects(objs[i], objs[i+1]) {
+          equal = false;
+        }
+      }
       equal = false;
     }
   });
@@ -97,7 +122,7 @@ var obj1 = {
 };
 
 var nestedArray = [1, obj];
-var array = [obj1, array];
+var array = [obj1, nestedArray];
 var obj2 = {
   h: array,
 };
@@ -129,31 +154,30 @@ var expected = {
     f: 5,
   },
   g: 6,
-  f: expectedArray,
+  h: expectedArray,
 };
 
 describe('functional-object-assign tests', () => {
   it ('should properly assign new values', () => {
-
-    expect(actual).to.deep.eql(expected);
+    expect(deepEqual(actual, expected)).to.be.true;
   });
-  it ('should not reference the src objects - when a src obj is mutated', () => {
-    obj.a = 99;
-    expect(actual).to.deep.eql(expected);
-    obj1.a = 99;
-    expect(actual).to.deep.eql(expected);
-  });
-  it ('should not reference the src objects - when a src array is mutated', () => {
-    obj2.h.push(99);
-    expect(actual).to.deep.eql(expected);
-    array.push(99);
-    expect(actual).to.deep.eql(expected);
-
-  });
-  it ('should not reference the src objects - when a nested src array is mutated', () => {
-    nestedArray.push(99);
-    expect(actual).to.deep.eql(expected);
-    obj2.h[1].push(99);
-    expect(actual).to.deep.eql(expected);
-  });
+  // it ('should not reference the src objects - when a src obj is mutated', () => {
+  //   obj.a = 99;
+  //   expect(deepEqual(actual, expected)).to.be.true;
+  //   obj1.a = 99;
+  //   expect(deepEqual(actual, expected)).to.be.true;
+  // });
+  // it ('should not reference the src objects - when a src array is mutated', () => {
+  //   obj2.h.push(99);
+  //   expect(deepEqual(actual, expected)).to.be.true;
+  //   array.push(99);
+  //   expect(deepEqual(actual, expected)).to.be.true;
+  //
+  // });
+  // it ('should not reference the src objects - when a nested src array is mutated', () => {
+  //   nestedArray.push(99);
+  //   expect(deepEqual(actual, expected)).to.be.true;
+  //   obj2.h[1].push(99);
+  //   expect(deepEqual(actual, expected)).to.be.true;
+  // });
 });
